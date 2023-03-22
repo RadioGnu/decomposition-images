f54ba2e17b28866688feffa8188f52bf3c1f92ae
VachetteCoco <franceverte54@gmail.com>
2023-03-22 16:09:18 +0100

    dico_galerie et selection carreaux_plus_proche
    fonction dico_galerie : récupère le dossier galerie et crée un dictionnaire contenant comme clé un indice et en valeur le tuple (chemin,image,val_moyenne)
    fonction couleur_proche: associe à chaque carreau les images dont les couleurs sont proches.

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ Created on Wed Mar 15 14:31:10 2023
 @author: DAMON Coline
 """
 import os
+import imageGalerie as im
 
-def dico_galerie(self):
+def dico_galerie(dossier):
     
     """
     Description:
@@ -14,38 +15,44 @@ def dico_galerie(self):
     
     """
     
-    liste_images = list(self.galerie)
+    liste_images = os.listdir(dossier)
     dico_galerie = {}
     for i in range(len(liste_images)) :
         if i not in dico_galerie :
-            image = image_galerie(liste_image[i])
-            dico_galerie[i] = image
-    self.dico_galerie = dico_galerie  
+            acces = dossier + "/" +liste_images[i]
+            image = im.imageGalerie(acces)
+            val = image.couleur_moyenne()
+            dico_galerie[i] = image, val
+            
+    return dico_galerie 
       
         
     
    
-def couleur_proche(self):
+def couleur_proche(val_moyenne, dico_galerie):
     """
-    Associe à la valeur moyenne des pixels de l'image initiale toutes les image dont la valeur moyenne est proche
+    Associe à la valeur moyenne de chaque pixel de l'image initiale toutes les image dont la valeur moyenne est proche
 
     Returns
     dictionnaire (ou liste?) des images choisies
     None.
 
     """
-    val_moy_init = self.valeur_moyenne
     couleur_proche = []
-    for image in self.dico_galerie.keys:
-        val_moy_image= image.self.couleur_moyenne() #renvoie le tuple moyen
-        for i in range (2):
-            ecart_couleur = int(abs(val_moy_init[i]-val_moy_image[i]))
-            liste_pixel_select = []
+    for image, val in dico_galerie.values():
+        liste_pixel_select = []
+        for couleur in range (3):
+            ecart_couleur = int(abs(val_moyenne[couleur]-val[couleur]))
+            
             if ecart_couleur <= 20:
                 liste_pixel_select.append(ecart_couleur)
                 if len(liste_pixel_select)==3:
                     couleur_proche.append(image)
-                
-            
+    return couleur_proche
+                    
+dossier = "C:/Users/DAMON Coline/Documents/GitHub/decomposition-images/gallerie"
+dico = dico_galerie(dossier)         
+val_moy = (12, 9,10)   
+print( couleur_proche(val_moy, dico) )        
             
         
